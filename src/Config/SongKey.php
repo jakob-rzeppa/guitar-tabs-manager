7a005c2dc0c801a0ec407bc0d1be6aa38e5def54
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-01-04 18:45:07 +0100

    Add Artist and Tab entities with relationships, and define SongKey enum

---
+++ src/Config/SongKey.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+enum SongKey: string
+{
+  case C = 'C';
+  case C_SHARP = 'C#';
+  case D_B = 'Db';
+  case D = 'D';
+  case D_SHARP = 'D#';
+  case E_B = 'Eb';
+  case E = 'E';
+  case F = 'F';
+  case F_SHARP = 'F#';
+  case G_B = 'Gb';
+  case G = 'G';
+  case G_SHARP = 'G#';
+  case A_B = 'Ab';
+  case A = 'A';
+  case A_SHARP = 'A#';
+  case B_B = 'Bb';
+  case B = 'B';
+}
---
+++ src/Entity/Tab.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TabRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TabRepository::class)]
+class Tab
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'tabs')]
+    private ?Artist $artist = null;
+
+    #[ORM\Column(length: 2, nullable: true)]
+    private ?string $songKey = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $capo = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getArtist(): ?Artist
+    {
+        return $this->artist;
+    }
+
+    public function setArtist(?Artist $artist): static
+    {
+        $this->artist = $artist;
+
+        return $this;
+    }
+
+    public function getSongKey(): ?string
+    {
+        return $this->songKey;
+    }
+
+    public function setSongKey(?string $songKey): static
+    {
+        $this->songKey = $songKey;
+
+        return $this;
+    }
+
+    public function getCapo(): ?int
+    {
+        return $this->capo;
+    }
+
+    public function setCapo(int $capo): static
+    {
+        $this->capo = $capo;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
