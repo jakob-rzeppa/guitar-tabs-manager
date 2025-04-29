2b6355c5284d57a60f478aa04ce75c5cc7ca6831
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-04-29 14:06:30 +0200

    fixed UserForm error that roles array could not be rendered by implementing the ChoiceType

--- src/Form/UserForm.php
+++ src/Form/UserForm.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,14 @@ class UserForm extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'ROLE_USER' => 'ROLE_USER',
+                    'ROLE_ADMIN' => 'ROLE_ADMIN',
+                ],
+                'multiple' => true,
+                'expanded' => true,
+            ])
             ->add('password')
         ;
     }
