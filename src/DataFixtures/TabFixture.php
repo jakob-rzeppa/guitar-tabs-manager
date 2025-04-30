c112c5febba5539d59ad75848ac51a7c624a6dbe
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-04-30 13:38:10 +0200

    added tags to TabFixture

--- src/DataFixtures/TabFixture.php
+++ src/DataFixtures/TabFixture.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Tab;
+use App\Entity\Tag;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,10 +11,15 @@ class TabFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $tag = new Tag();
+        $tag->setName('Tag 1');
+        $manager->persist($tag);
+
         $tab = new Tab();
         $tab->setTitle('Tab 1');
         $tab->setCapo(0);
         $tab->setContent('This is the content of Tab 1.');
+        $tab->addTag($tag);
         $manager->persist($tab);
 
         $manager->flush();
