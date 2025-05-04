a7159745efd9f7139ea00846cf212f250cfe9c80
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-05-04 17:26:22 +0200

    added user fixture

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixture extends Fixture
+{
+
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    ) {}
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('rzeppa.jakob@gmail.com');
+        $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user,
+                'password'
+            )
+        );
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
