d3a57f3a8efa73c6ad8d77ecf7496673271d0931
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-06-22 16:42:37 +0200

    Revert "created next js api platform client"
    This reverts commit ac71d9e3b963a46f6ff884adee36de7b410f10ce.

--- pwa/types/Artist.ts
+++
@@ -1,9 +0,0 @@
-import { Item } from "./item";
-
-export class Artist implements Item {
-  public "@id"?: string;
-
-  constructor(_id?: string, public name?: string, public tabs?: string[]) {
-    this["@id"] = _id;
-  }
-}
--- pwa/types/Tag.ts
+++
@@ -1,9 +0,0 @@
-import { Item } from "./item";
-
-export class Tag implements Item {
-  public "@id"?: string;
-
-  constructor(_id?: string, public name?: string, public tabs?: string[]) {
-    this["@id"] = _id;
-  }
-}
