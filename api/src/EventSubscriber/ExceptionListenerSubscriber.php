0e956986e27d2d659e60855e77571f5361b8a13e
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-06-23 14:50:06 +0200

    created a ExceptionSubscriber to handle Exceptions

---
+++ api/src/EventSubscriber/ExceptionListenerSubscriber.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ExceptionListenerSubscriber implements EventSubscriberInterface
+{
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder()]);
+
+        // You get the exception object from the received event
+        $exception = $event->getThrowable();
+
+        // Customize your response object to display the exception details
+        $responseBody = [
+            'message' => $exception->getMessage(),
+            'code' => $exception->getCode(),
+            'traces' => $exception->getTrace()
+        ];
+
+        $jsonResponseBody = $serializer->serialize($responseBody, 'json');
+
+        $response = JsonResponse::fromJsonString($jsonResponseBody);
+
+        // HttpExceptionInterface is a special type of exception that
+        // holds status code and header details
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        // sends the modified response object to the event
+        $event->setResponse($response);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ExceptionEvent::class => 'onKernelException',
+        ];
+    }
+}
