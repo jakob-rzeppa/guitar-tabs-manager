66a8f421a7420302d2b465ef2311fd8bfa88c475
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-06-23 11:33:04 +0200

    added entities and repositories

---
+++ api/src/Entity/Tab.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TabRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TabRepository::class)]
+class Tab
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $capo = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    /**
+     * @var Collection<int, Tag>
+     */
+    #[ORM\ManyToMany(targetEntity: Tag::class, inversedBy: 'tabs')]
+    private Collection $tags;
+
+    #[ORM\ManyToOne(inversedBy: 'tabs')]
+    private ?Artist $artist = null;
+
+    public function __construct()
+    {
+        $this->tags = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCapo(): ?int
+    {
+        return $this->capo;
+    }
+
+    public function setCapo(int $capo): static
+    {
+        $this->capo = $capo;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tag>
+     */
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
+
+    public function addTag(Tag $tag): static
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags->add($tag);
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): static
+    {
+        $this->tags->removeElement($tag);
+
+        return $this;
+    }
+
+    public function getArtist(): ?Artist
+    {
+        return $this->artist;
+    }
+
+    public function setArtist(?Artist $artist): static
+    {
+        $this->artist = $artist;
+
+        return $this;
+    }
+}
---
+++ api/src/Entity/Tag.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TagRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TagRepository::class)]
+class Tag
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    /**
+     * @var Collection<int, Tab>
+     */
+    #[ORM\ManyToMany(targetEntity: Tab::class, mappedBy: 'tags')]
+    private Collection $tabs;
+
+    public function __construct()
+    {
+        $this->tabs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tab>
+     */
+    public function getTabs(): Collection
+    {
+        return $this->tabs;
+    }
+
+    public function addTab(Tab $tab): static
+    {
+        if (!$this->tabs->contains($tab)) {
+            $this->tabs->add($tab);
+            $tab->addTag($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTab(Tab $tab): static
+    {
+        if ($this->tabs->removeElement($tab)) {
+            $tab->removeTag($this);
+        }
+
+        return $this;
+    }
+}
