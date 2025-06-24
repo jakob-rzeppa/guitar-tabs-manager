28fadb0e29ec3ed8a9fb6b3e3f12ab66397cbfd8
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-06-24 21:30:15 +0200

    created a loading placeholder

---
+++ web/src/components/LoadingPlaceholder.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="relative w-full h-full flex items-center justify-center">
+    <span class="loading loading-spinner min-w-10 w-[4%]" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
