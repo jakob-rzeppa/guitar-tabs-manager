3a2f95d50576c2a51db1f064860688ac58c5daf0
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-06-25 12:54:22 +0200

    implemented fetching of tab in TabView.vue

--- web/src/views/TabView.vue
+++ web/src/views/TabView.vue
@@ -1,8 +1,33 @@
 <script setup lang="ts">
+
+import {useRoute} from "vue-router";
+import type {APIResponse, Tab} from "@/types/types.ts";
+import {ref, watch} from "vue";
+import type {AxiosResponse} from "axios";
+import {fetchFromAPI} from "@/services/api.ts";
+
+const route = useRoute()
+
+const loading = ref(false)
+const response = ref<AxiosResponse<APIResponse<Tab>> | null>(null)
+const error = ref<string | null>(null)
+
+watch(
+    () => route.params.id,
+    (newId) => {
+      let id = newId;
+      if (Array.isArray(id)) {
+        id = id[0];
+      }
+
+      fetchFromAPI('/tab/' + id, 'GET', {loading, response, error}).then();
+    }, { immediate: true }
+)
+
 </script>
 
 <template>
   <main>
-    TabView: {{ $route.params.id }}
+    TabView: {{ response?.data }}
   </main>
 </template>
