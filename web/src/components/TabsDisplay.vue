24c2ede1ff21cb0018ff196d82f1694fd62cbdaf
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-06-26 20:28:29 +0200

    created TabsDisplay.vue to improve readability in TabSearchView.vue

---
+++ web/src/components/TabsDisplay.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type {Tab} from "@/types/types.ts";
+import {useRouter} from "vue-router";
+
+const router = useRouter()
+
+interface Props {
+  tabs: Tab[]
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <ul class="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2 p-8">
+    <li v-for="tab in tabs" class="card shadow-sm bg-base-200 cursor-pointer hover:shadow-lg transition-shadow" @click="router.push({name: 'tab', params: {id: tab.id}});">
+      <div class="card-body">
+        <h2 class="card-title">{{tab.title}} <span v-if="tab.artist !== null" class="text-primary">by</span> <span v-if="tab.artist !== null">{{tab.artist.name}}</span></h2>
+        <ul class="flex flex-row flex-wrap gap-1.5">
+          <li v-for="tag in tab.tags" class="badge badge-secondary">
+            {{tag.name}}
+          </li>
+        </ul>
+      </div>
+    </li>
+  </ul>
+</template>
--- web/src/views/TabSearchView.vue
+++ web/src/views/TabSearchView.vue
@@ -7,6 +7,7 @@ import ErrorDisplay from "@/components/ErrorDisplay.vue";
 import LoadingPlaceholder from "@/components/LoadingPlaceholder.vue";
 import {useRouter} from "vue-router";
 import ContentWrapper from "@/components/ContentWrapper.vue";
+import TabsDisplay from "@/components/TabsDisplay.vue";
 
 const router = useRouter()
 
@@ -22,17 +23,6 @@ fetchFromAPI<Tab[]>('/tab', 'GET', {loading, response, error}).then();
     <ErrorDisplay v-if="error !== null" :message="error" />
     <LoadingPlaceholder v-else-if="loading" />
     <ErrorDisplay v-else-if="response === null || response.data.content === undefined" :message="'Data is not available.'" />
-    <ul v-else class="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2 p-8">
-      <li v-for="tab in response.data.content" class="card shadow-sm bg-base-200 cursor-pointer hover:shadow-lg transition-shadow" @click="router.push({name: 'tab', params: {id: tab.id}});">
-        <div class="card-body">
-          <h2 class="card-title">{{tab.title}} <span v-if="tab.artist !== null" class="text-primary">by</span> <span v-if="tab.artist !== null">{{tab.artist.name}}</span></h2>
-          <ul class="flex flex-row flex-wrap gap-1.5">
-            <li v-for="tag in tab.tags" class="badge badge-secondary">
-              {{tag.name}}
-            </li>
-          </ul>
-        </div>
-      </li>
-    </ul>
+    <TabsDisplay v-else :tabs="response.data.content" />
   </ContentWrapper>
 </template>
