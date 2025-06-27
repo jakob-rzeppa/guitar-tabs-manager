620fa569f116da2df025e572185ab5e66fd91105
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-06-27 14:06:34 +0200

    created input for tab search filters (artist / tags)

--- web/src/views/TabSearchView.vue
+++ web/src/views/TabSearchView.vue
@@ -34,12 +34,28 @@ function orderTabs(event: Event) {
 
 <template>
   <ContentWrapper>
-    <div>
-      <label class="input box-border w-[calc(100%-4rem)] m-8">
-        <span class="label">Tab Search:</span>
+    <div class="px-14 pt-8">
+      <h1 class="text-4xl font-bold mx-auto w-fit mb-8">Search for Tabs</h1>
+      <label class="input box-border w-full">
+        <span class="label font-bold">Search</span>
         <input type="text" placeholder="Type here" @input="orderTabs" />
       </label>
+      <div tabindex="0" class="collapse collapse-arrow">
+        <input type="checkbox" />
+        <div class="collapse-title font-semibold">Filter</div>
+        <div class="collapse-content flex flex-col gap-4">
+          <label class="input box-border w-full">
+            <span class="label">Artist</span>
+            <input type="text" placeholder="Type here" />
+          </label>
+          <label class="input box-border w-full">
+            <span class="label">Tags</span>
+            <input type="text" placeholder="Type here" />
+          </label>
+        </div>
+      </div>
     </div>
+    <div class="divider"></div>
     <ErrorDisplay v-if="error !== null" :message="error" />
     <LoadingPlaceholder v-else-if="loading" />
     <ErrorDisplay v-else-if="response === null || response.data.content === undefined" :message="'Data is not available.'" />
