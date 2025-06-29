5836bbbcc377c4d53a7c5e63a095b742f8c2d1fd
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-06-29 14:04:57 +0200

    implemented error handling for selecting tags

--- web/src/components/SelectTags.vue
+++ web/src/components/SelectTags.vue
@@ -14,17 +14,18 @@ const error = ref<string | null>(null)
 fetchFromAPI<Tag[]>('/tag', 'GET', {loading, response, error}).then();
 
 const activeTags = ref<Tag[]>([])
+const errorMessage = ref<string | null>(null)
 
 function addActiveTag(event: Event) {
   const inputElement = event.currentTarget as HTMLInputElement
 
   if (inputElement.value === '') {
-    //TODO display error
+    displayError('Please enter a tag')
     return
   }
 
   if (!response || !response.value || !response.value.data.content) {
-    //TODO display error
+    displayError('Something went wrong. Please try reloading the page.');
     return
   }
 
@@ -33,38 +34,47 @@ function addActiveTag(event: Event) {
   )
 
   if (!tagToAdd) {
-    //TODO display error
+    displayError('No tag found with this name')
     return
   }
 
   if (activeTags.value.includes(tagToAdd)) {
-    //TODO display error
+    displayError('Tag already selected')
     return
   }
 
   activeTags.value.push(tagToAdd)
 
   inputElement.value = ''
+  removeError()
 }
 
 function removeActiveTag(event: Event) {
   const element = event.currentTarget as HTMLInputElement
 
   if (!response || !response.value || !response.value.data.content) {
-    //TODO display error
+    displayError('Something went wrong. Please try reloading the page.');
     return
   }
 
   const indexToRemove = activeTags.value.findIndex((tag) => tag.id === parseInt(element.value))
 
   if (!indexToRemove) {
-    //TODO display error
+    displayError('Something went wrong.');
     return
   }
 
   activeTags.value.splice(indexToRemove, 1)
 
-  console.log(activeTags.value)
+  removeError()
+}
+
+function displayError(message: string) {
+  errorMessage.value = message
+}
+
+function removeError() {
+  errorMessage.value = null
 }
 </script>
 
@@ -73,7 +83,7 @@ function removeActiveTag(event: Event) {
   <ErrorDisplay v-else-if="error" :message="error" />
   <ErrorDisplay v-else-if="!response || !response.data.content" message="Something went wrong while retrieving artists." />
   <div v-else class="">
-    <label class="input box-border w-full">
+    <label class="input box-border w-full" :class="{ 'input-error': errorMessage }">
       <span class="label">Tags</span>
       <span v-if="activeTags.length > 0" class="label">
         <span v-for="tag in activeTags" class="tooltip" data-tip="deselect">
@@ -87,5 +97,6 @@ function removeActiveTag(event: Event) {
         <option v-for="tag in response.data.content.filter( (el) => !activeTags.includes( el ))" :value="tag.name" />
       </datalist>
     </label>
+    <div class="mt-1 text-error" :class="{ 'hidden': !errorMessage }">{{errorMessage}}</div>
   </div>
 </template>
