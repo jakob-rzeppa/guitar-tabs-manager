4be8e1505de6fceafeb9a9285b6ed35d78e2ac74
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-07-17 19:05:44 +0200

    moved tab edit form in seperate component to later reuse in tab creation

---
+++ web/src/components/TabForm.vue
@@ -0,0 +1,215 @@
+<script setup lang="ts">
+
+import LoadingPlaceholder from "@/components/LoadingPlaceholder.vue";
+import ErrorDisplay from "@/components/ErrorDisplay.vue";
+import SelectArtist from "@/components/SelectArtist.vue";
+import SelectTags from "@/components/SelectTags.vue";
+import {useRoute, useRouter} from "vue-router";
+import {useTabByIdStore} from "@/stores/tabById.ts";
+import {computed, onMounted, ref, toRaw} from "vue";
+import type {APIResponse, Tab} from "@/types/types.ts";
+import api, {useApi} from "@/services/api.ts";
+
+const route = useRoute()
+const router = useRouter()
+const tabByIdStore = useTabByIdStore()
+
+const tabId = computed(() => route.params.id as string)
+const currentTab = computed(() => tabByIdStore.tabs[tabId.value])
+
+const localTab = ref<Tab | null>(null)
+function saveTab() {
+  if (!localTab.value) {
+    console.error("Tab not found")
+    return
+  }
+
+  if (!currentTab.value.content) {
+    console.error("Current tab not found");
+    return
+  }
+
+  const title = currentTab.value.content.title !== localTab.value.title ? localTab.value.title : null
+  const capo = currentTab.value.content.capo !== localTab.value.capo ? localTab.value.capo : null
+  const content = currentTab.value.content.content !== localTab.value.content ? localTab.value.content : null
+
+  const artistId = currentTab.value.content.artist?.id !== localTab.value.artist.id
+      ? localTab.value.artist.id
+      : null
+
+  let tagIds: number[] | null = localTab.value.tags.map(tag => tag.id).sort((a, b) => a - b)
+  const oldTagIds = currentTab.value.content.tags.map(tag => tag.id).sort((a, b) => a - b)
+
+  if (tagIds.length === oldTagIds.length && tagIds.every((val, i) => val === oldTagIds[i])) {
+    tagIds = null;
+  }
+
+  const dto = Object.assign({},
+      title && {title},
+      capo && {capo},
+      content && {content},
+      artistId && {artist_id: artistId},
+      tagIds && {tag_ids: tagIds},
+  );
+
+  if (Object.keys(dto).length === 0) {
+    console.log("No tab changes to save.")
+    router.push({name: 'tab', params: {id: tabId.value}})
+    return
+  }
+
+  console.log("Saving tab changes: ", dto)
+  tabByIdStore.updateTab(currentTab.value.content.id.toString(), dto)
+
+  router.push({name: 'tab', params: {id: tabId.value}})
+}
+
+onMounted(async () => {
+  await tabByIdStore.fetchTab(tabId.value)
+  localTab.value = structuredClone(toRaw(currentTab.value.content!))
+})
+
+// FORMAT
+const formatLoading = ref<boolean>(false);
+const formatError = ref<string | null>(null);
+const formatResponse = ref<APIResponse<string> | null>(null)
+
+function formatTab() {
+  if (!localTab.value) {
+    console.error("Tab not found for formating.")
+    return
+  }
+
+  const prevTabContent = localTab.value.content;
+
+  useApi({
+    loading: formatLoading,
+    error: formatError,
+    response: formatResponse,
+    apiCall: () => api.post('/tabs/format', {content: prevTabContent})
+  }).then(() => {
+    if (!formatResponse.value || !formatResponse.value.content) {
+      console.error("API Format Response not found.")
+      return
+    }
+    if (!localTab.value) {
+      console.error("Tab not found for formating.")
+      return
+    }
+    localTab.value.content = formatResponse.value.content;
+    console.log('Tab formated successfully.');
+  })
+}
+
+// TRANSPOSE
+const transposeDir = ref<'up' | 'down'>('down')
+const transposeLoading = ref<boolean>(false);
+const transposeError = ref<string | null>(null);
+const transposeResponse = ref<APIResponse<string> | null>(null)
+const transposeAndMoveCapo = ref(true)
+
+function transposeTab() {
+  if (!localTab.value) {
+    console.error("Tab not found for transposing.")
+    return
+  }
+
+  const prevTabContent = localTab.value.content;
+
+  useApi({
+    loading: transposeLoading,
+    error: transposeError,
+    response: transposeResponse,
+    apiCall: () => api.post('/tabs/transpose', {content: prevTabContent, dir: transposeDir.value})
+  }).then(() => {
+    if (!transposeResponse.value || !transposeResponse.value.content) {
+      console.error("API Transpose Response not found.")
+      return
+    }
+    if (!localTab.value) {
+      console.error("Tab not found for transposing.")
+      return
+    }
+    if (transposeAndMoveCapo.value === true) {
+      if (transposeDir.value === 'up') {
+        localTab.value.capo--
+      } else if (transposeDir.value === 'down') {
+        localTab.value.capo++
+      } else {
+        console.error("Something went wrong when changing the capo.")
+        return
+      }
+    }
+    localTab.value.content = transposeResponse.value.content;
+    console.log('Tab transposed ' + (transposeAndMoveCapo.value === true && 'and capo changed ') + 'successfully.');
+  })
+}
+
+function discardChanges() {
+  if (!localTab.value || !currentTab.value.content) {
+    console.error("Tab not found when discarding changes.")
+    router.push({name: 'tab', params: {id: tabId.value}})
+    return
+  }
+
+  let tagIds: number[] | null = localTab.value.tags.map(tag => tag.id).sort((a, b) => a - b)
+  const oldTagIds = currentTab.value.content.tags.map(tag => tag.id).sort((a, b) => a - b)
+
+  if (
+      currentTab.value.content.title !== localTab.value.title ||
+      currentTab.value.content.capo !== localTab.value.capo ||
+      currentTab.value.content.content !== localTab.value.content ||
+      currentTab.value.content.artist?.id !== localTab.value.artist.id ||
+      tagIds.length !== oldTagIds.length || tagIds.every((val, i) => val !== oldTagIds[i])
+  ) {
+    alert('Are you sure you want to discard all changes?')
+  }
+
+  router.push({name: 'tab', params: {id: tabId.value}})
+}
+</script>
+
+<template>
+  <LoadingPlaceholder v-if="tabByIdStore.loading" />
+  <ErrorDisplay v-else-if="tabByIdStore.error !== null" :message="tabByIdStore.error" />
+  <ErrorDisplay v-else-if="!currentTab || !currentTab.content" message="No content." />
+  <ErrorDisplay v-else-if="localTab === null" message="Something went wrong while retrieving tabs." />
+  <div v-else>
+    <div class="flex flex-col gap-4 pt-4">
+      <label class="input w-full">
+        <span class="label">title</span>
+        <input type="text" :value="localTab.title" @input="event => localTab!.title = (event.target as HTMLInputElement).value" />
+      </label>
+      <label class="input w-full">
+        <span class="label">capo</span>
+        <input type="number" :value="localTab.capo" @input="event => localTab!.capo = parseInt((event.target as HTMLInputElement).value)" />
+      </label>
+      <SelectArtist :artist="currentTab.content.artist" @select="artist => localTab!.artist = artist" />
+      <SelectTags :initial-tags="currentTab.content.tags" @select="tags => localTab!.tags = tags" />
+      <div class="flex flex-row gap-4">
+        <button class="btn btn-error w-fit" @click="discardChanges">Discard Changes</button>
+        <button class="btn btn-success w-fit" @click="saveTab">Save</button>
+      </div>
+    </div>
+    <div class="divider"></div>
+    <LoadingPlaceholder v-if="formatLoading || transposeLoading" />
+    <ErrorDisplay v-else-if="formatError !== null" :message="formatError" />
+    <ErrorDisplay v-else-if="transposeError !== null" :message="transposeError" />
+    <div v-else class="flex flex-col gap-4">
+      <button class="btn w-fit" @click="formatTab">Format</button>
+      <div class="flex gap-2">
+        <select class="select w-max" v-model="transposeAndMoveCapo">
+          <option :value="true">change Capo</option>
+          <option :value="false">transpose</option>
+        </select>
+        <select class="select w-min" v-model="transposeDir">
+          <option>up</option>
+          <option>down</option>
+        </select>
+        <button class="btn w-fit" @click="transposeTab">{{ transposeAndMoveCapo ? 'Change Capo' : 'Transpose'}}</button>
+      </div>
+      <p class="text-info hidden">Dont forget to save changes.</p>
+      <textarea class="textarea w-full h-screen" :value="localTab.content" @input="event => localTab!.content = (event.target as HTMLInputElement).value"></textarea>
+    </div>
+  </div>
+</template>
