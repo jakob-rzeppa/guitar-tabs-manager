bfbb5cb33ed0c52b8574abb827e1a3eda4aebcc9
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-11-15 16:14:24 +0100

    fixed TabSearchView

--- web/src/views/TabSearchView.vue
+++ web/src/views/TabSearchView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref, toRaw } from 'vue';
-import type { APIResponse, Artist, Tab, Tag } from '@/types/types.ts';
+import { ref, toRaw, watch } from 'vue';
+import type { APIResponse, Artist, Tab, TabListItem, Tag } from '@/types/types.ts';
 import ErrorDisplay from '@/components/ErrorDisplay.vue';
 import LoadingPlaceholder from '@/components/LoadingPlaceholder.vue';
 import ContentWrapper from '@/components/ContentWrapper.vue';
@@ -8,80 +8,58 @@ import TabsDisplay from '@/components/TabsDisplay.vue';
 import calculateSimilarity from '@/services/calculateSimilarity.ts';
 import SelectArtist from '@/components/SelectArtist.vue';
 import SelectTags from '@/components/SelectTags.vue';
-import { useApi } from '@/services/api.ts';
-import api from '@/services/api.ts';
+import { useTabsStore } from '@/stores/tabsStore';
 
-const loading = ref(false);
-const error = ref<string | null>(null);
-const response = ref<APIResponse<Tab[]> | null>(null);
-const apiCall = () => api.get('/tabs');
+const tabsStore = useTabsStore();
 
-const displayedTabs = ref<Tab[]>([]);
+tabsStore.fetchAllTabs();
 
-useApi({ loading, error, response, apiCall }).then(() => {
-    if (!response.value || !response.value.content) {
-        throw new Error('Response object is empty.');
-    }
-    displayedTabs.value = structuredClone(toRaw(response.value.content));
-});
+const searchValue = ref<string>('');
+const artistIdFilter = ref<number | null>(null);
+const tagIdsFilter = ref<number[]>([]);
 
-function orderTabs(event: Event) {
-    const searchValue = (event.target as HTMLInputElement).value;
+const displayedTabs = ref<TabListItem[]>([]);
 
-    if (!response.value || !response.value.content) {
-        throw new Error('Response object is empty.');
-    }
+watch(
+    () => [tabsStore.tabsList, searchValue.value, artistIdFilter.value, tagIdsFilter.value],
+    () => {
+        if (artistIdFilter.value === null) {
+            displayedTabs.value = [...tabsStore.tabsList];
+        } else {
+            displayedTabs.value = tabsStore.tabsList.filter((tab) => {
+                return tab.artist !== null && tab.artist.id === artistIdFilter.value;
+            });
+        }
 
-    displayedTabs.value.sort((a, b) => {
-        const titleA = a.title.toLowerCase();
-        const titleB = b.title.toLowerCase();
-        return calculateSimilarity(searchValue, titleB) - calculateSimilarity(searchValue, titleA);
-    });
+        if (tagIdsFilter.value.length > 0) {
+            displayedTabs.value = displayedTabs.value.filter((item) => {
+                if (item.tags.length === 0) {
+                    return false;
+                }
 
-    // needs to be sorted as well, so that the right order is present when the artist or tabs are changed
-    response.value.content.sort((a, b) => {
-        const titleA = a.title.toLowerCase();
-        const titleB = b.title.toLowerCase();
-        return calculateSimilarity(searchValue, titleB) - calculateSimilarity(searchValue, titleA);
-    });
-}
+                const itemTagIds = item.tags.map((tag) => tag.id);
+                return tagIdsFilter.value.every((filterTagId) => itemTagIds.includes(filterTagId));
+            });
+        }
+
+        displayedTabs.value.sort((a, b) => {
+            const titleA = a.title.toLowerCase();
+            const titleB = b.title.toLowerCase();
+            return (
+                calculateSimilarity(searchValue.value, titleB) -
+                calculateSimilarity(searchValue.value, titleA)
+            );
+        });
+    },
+    { immediate: true },
+);
 
 function filterByArtist(artist: Artist | null) {
-    if (!response.value || !response.value.content) {
-        throw new Error('Response object is empty.');
-    }
-
-    if (!artist) {
-        displayedTabs.value = response.value.content;
-        return;
-    }
-
-    displayedTabs.value = response.value.content.filter((item) => {
-        if (item.artist === null) {
-            return false;
-        }
-        return item.artist.id === artist.id;
-    });
+    artistIdFilter.value = artist?.id ?? null;
 }
 
 function filterByTags(tags: Tag[]) {
-    if (!response.value || !response.value.content) {
-        throw new Error('Response object is empty.');
-    }
-
-    if (tags.length === 0) {
-        displayedTabs.value = response.value.content;
-        return;
-    }
-
-    displayedTabs.value = response.value.content.filter((item) => {
-        if (item.tags.length === 0) {
-            return false;
-        }
-
-        const itemTagIds = item.tags.map((tag) => tag.id);
-        return tags.every((filterTag) => itemTagIds.includes(filterTag.id));
-    });
+    tagIdsFilter.value = tags.map((tag) => tag.id);
 }
 </script>
 
@@ -91,7 +69,7 @@ function filterByTags(tags: Tag[]) {
             <h1 class="text-4xl font-bold mx-auto w-fit mb-8">Search for Tabs</h1>
             <label class="input box-border w-full">
                 <span class="label font-bold">Search</span>
-                <input type="text" placeholder="Type here" @input="orderTabs" />
+                <input type="text" placeholder="Type here" v-model="searchValue" />
             </label>
             <div tabindex="0" class="collapse collapse-arrow">
                 <input type="checkbox" />
@@ -103,8 +81,8 @@ function filterByTags(tags: Tag[]) {
             </div>
         </div>
         <div class="divider"></div>
-        <ErrorDisplay v-if="error !== null" :message="error" />
-        <LoadingPlaceholder v-else-if="loading" />
+        <LoadingPlaceholder v-if="tabsStore.loading" />
+        <ErrorDisplay v-else-if="tabsStore.error !== null" :message="tabsStore.error" />
         <TabsDisplay v-else :tabs="displayedTabs" />
     </ContentWrapper>
 </template>
