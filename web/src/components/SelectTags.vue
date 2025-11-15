39608edb1cb6c443723d4d6e02f80d54b30f3432
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-11-15 22:42:12 +0100

    updated select tags style

--- web/src/components/SelectTags.vue
+++ web/src/components/SelectTags.vue
@@ -58,41 +58,76 @@ function removeActiveTag(id?: number) {
         v-else-if="!tagsStore.tags || tagsStore.tags.length === 0"
         message="Something went wrong while retrieving tags."
     />
-    <div v-else class="">
-        <label class="input box-border w-full">
-            <span class="label">Tags</span>
-            <span v-if="model.length > 0" class="label">
-                <span
-                    v-for="tag in model"
-                    class="bg-primary px-3 py-1 rounded-lg group hover:brightness-80 cursor-pointer"
-                    @click="() => removeActiveTag(tag.id)"
-                >
-                    {{ tag.name }}
-                </span>
-            </span>
-            <input
-                type="text"
-                list="tags"
-                placeholder="Type here"
-                v-model="inputValue"
-                @keypress.enter.stop="addActiveTag"
-                @keydown.delete.stop="
-                    () => {
-                        if (inputValue === '') removeActiveTag();
-                    }
-                "
-            />
-            <button class="btn btn-circle btn-xs btn-outline btn-primary" @click="addActiveTag">
-                +
-            </button>
-            <datalist id="tags">
-                <option
-                    v-for="tag in tagsStore.tags.filter(
-                        (el: Tag) => model.findIndex((m) => m.id === el.id) === -1,
-                    )"
-                    :value="tag.name"
-                />
-            </datalist>
+    <div v-else>
+        <label class="label">
+            <span class="label-text text-base font-semibold">Tags</span>
+            <span class="label-text-alt">Press Enter to add</span>
         </label>
+
+        <div class="join w-full">
+            <label class="input box-border w-full join-item">
+                <span v-if="model.length > 0" class="label">
+                    <span
+                        v-for="tag in model"
+                        :key="tag.id"
+                        class="badge badge-primary gap-2 cursor-pointer hover:badge-secondary transition-colors"
+                        @click="() => removeActiveTag(tag.id)"
+                    >
+                        {{ tag.name }}
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-4 w-4"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    </span>
+                </span>
+                <input
+                    type="text"
+                    list="tags"
+                    placeholder="Type here"
+                    v-model="inputValue"
+                    @keypress.enter.stop="addActiveTag"
+                    @keydown.delete.stop="
+                        () => {
+                            if (inputValue === '') removeActiveTag();
+                        }
+                    "
+                />
+            </label>
+            <button class="btn btn-primary join-item" @click="addActiveTag">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 4v16m8-8H4"
+                    />
+                </svg>
+            </button>
+        </div>
+        <datalist id="tags">
+            <option
+                v-for="tag in tagsStore.tags.filter(
+                    (el: Tag) => model.findIndex((m) => m.id === el.id) === -1,
+                )"
+                :key="tag.id"
+                :value="tag.name"
+            />
+        </datalist>
     </div>
 </template>
