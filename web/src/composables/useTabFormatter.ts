fdf4e76303e765390b163445f363c0e3d86e13db
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-11-16 22:22:36 +0100

    updated formatter and transposer to use new dtos

--- web/src/composables/useTabFormatter.ts
+++ web/src/composables/useTabFormatter.ts
@@ -1,24 +1,24 @@
 import api, { useApi } from '@/services/api';
-import type { APIResponse } from '@/types/types';
+import type { ApiResponse, FormatTabDto } from '@/types/dtos';
 import { ref, watch } from 'vue';
 
 export const useTabFormatter = () => {
     const formatLoading = ref<boolean>(false);
     const formatError = ref<string | null>(null);
-    const formatResponse = ref<APIResponse<string> | null>(null);
+    const formatResponse = ref<ApiResponse<FormatTabDto> | null>(null);
 
     const formattedTabContent = ref<string | null>(null);
 
     watch(formatResponse, (newValue) => {
         if (newValue && newValue.content) {
-            formattedTabContent.value = newValue.content;
+            formattedTabContent.value = newValue.content.content;
         } else {
             formattedTabContent.value = null;
         }
     });
 
     function formatTab(content: string) {
-        useApi({
+        useApi<FormatTabDto>({
             loading: formatLoading,
             error: formatError,
             response: formatResponse,
--- web/src/composables/useTabTransposer.ts
+++ web/src/composables/useTabTransposer.ts
@@ -1,24 +1,24 @@
 import api, { useApi } from '@/services/api';
-import type { APIResponse } from '@/types/types';
+import type { ApiResponse, TransposeTabDto } from '@/types/dtos';
 import { ref, watch } from 'vue';
 
 export const useTabTransposer = () => {
     const transposeLoading = ref<boolean>(false);
     const transposeError = ref<string | null>(null);
-    const transposeResponse = ref<APIResponse<string> | null>(null);
+    const transposeResponse = ref<ApiResponse<TransposeTabDto> | null>(null);
 
     const transposedTabContent = ref<string | null>(null);
 
     watch(transposeResponse, (newValue) => {
         if (newValue && newValue.content) {
-            transposedTabContent.value = newValue.content;
+            transposedTabContent.value = newValue.content.content;
         } else {
             transposedTabContent.value = null;
         }
     });
 
     const transposeTab = (content: string, transposeDir: 'up' | 'down') => {
-        useApi({
+        useApi<TransposeTabDto>({
             loading: transposeLoading,
             error: transposeError,
             response: transposeResponse,
