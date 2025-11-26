01afd04b6d2ddb7784c31d86d69b4c58dab2ed2e
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-11-26 13:46:42 +0100

    api: updated ArtistDto to have the functions fromArtist and toArray

--- api/src/Dto/ArtistDto.php
+++ api/src/Dto/ArtistDto.php
@@ -2,10 +2,28 @@
 
 namespace App\Dto;
 
+use App\Entity\Artist;
+
 readonly class ArtistDto
 {
     public function __construct(
         public int $id,
         public string $name,
     ) {}
+
+    public static function fromArtist(Artist $artist): self
+    {
+        return new self(
+            id: $artist->getId(),
+            name: $artist->getName(),
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+        ];
+    }
 }
